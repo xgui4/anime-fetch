677e2e850e81821d27c97605d6b0eebd34c8777a
xgui4 <134389196+xgui4@users.noreply.github.com>
2026-02-22 18:45:20 -0500

    update rhe installer + the gui

--- gui/src/app.cpp
+++ gui/src/app.cpp
@@ -1,34 +1,79 @@
 #include <QtWidgets/QDialog>
 #include <QtWidgets/QApplication>
 #include <qboxlayout.h>
+#include <qdebug.h>
 #include <qimage.h>
 #include <qlabel.h>
 #include <qmainwindow.h>
 #include <qnamespace.h>
+#include <qobject.h>
 #include <qpixmap.h>
 #include <qsize.h>
+#include <qstringview.h>
+#include <qsysinfo.h>
 #include <qwidget.h>
 #include <QLabel>
 #include <QBoxLayout>
+#include <QDir>
+#include <QDebug>
 
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
     QMainWindow * mainWindow = new QMainWindow();
 
+    mainWindow->setMinimumHeight(500); 
+    mainWindow->setMinimumWidth(500); 
+
     QWidget *centralWidget = new QWidget(mainWindow);
 
-    QBoxLayout *  box = new QHBoxLayout(centralWidget); 
-    QLabel * label = new QLabel("Hello World"); 
+    QBoxLayout * box = new QHBoxLayout(centralWidget); 
 
-    QPixmap image = QPixmap("/home/xgui4/.anime-fetch/icons/appicon.png");
+    box->setSpacing(5);
+
+    QSysInfo systemInfo; 
+
+    const QString HOSTNAME_STR = systemInfo.machineHostName(); 
+    const QString KERNEL_NAME_STR = systemInfo.kernelType() + " " + systemInfo.kernelVersion(); 
+    const QString OS_NAME_STR = systemInfo.prettyProductName(); 
+
+    QLabel * label = new QLabel("Hostname: " + HOSTNAME_STR + "\n" + "Kernel: " + KERNEL_NAME_STR + "\n" + "OS: " + OS_NAME_STR); 
+
+    QString os_tan_img_path; 
+
+    // QString logo_img_path; 
+
+    #ifdef __win32__
+        os_tan_img_path = ".anime-fetch/images/os-tan/windows/win11-os-tan-temp.png"; 
+    #elif __apple__
+        os_tan_img_path =  ".anime-fetch/images/os-tan/mac/system-tan.png"; 
+    #elif __linux__
+        os_tan_img_path = ".anime-fetch/images/os-tan/linux/arch-1-tan.png";
+    #elif __FreeBSD__
+        os_tan_img_path = ".anime-fetch/images/os-tan/others/free-bsd-tan.png";
+    #elif __unix__
+        os_tan_img_path = ".anime-fetch/images/os-tan/others/solaris-tan.png";
+    #else
+        os_tan_img_path = ".anime-fetch/images/os-tan/windows/os-tan7.png"; 
+    #endif
+
+    const QString HOME_PATH = QDir::homePath(); 
+
+    qDebug() << HOME_PATH + "/" + os_tan_img_path << Qt::endl;
+
+    #ifdef __win32__
+        QPixmap image = QPixmap(    HOME_PATH + "\\" + os_tan_img_path);
+    #else
+        QPixmap image = QPixmap(    HOME_PATH + "/" + os_tan_img_path);
+    #endif
+
     QLabel* imgLabel = new QLabel();
     imgLabel->setPixmap(image); 
 
     box->addWidget(label); 
     box->addWidget(imgLabel); 
 
-    label->setAlignment(Qt::AlignCenter); 
+    mainWindow->setCentralWidget(centralWidget);
 
     mainWindow->setWindowTitle("Anime Fetch");
     mainWindow->show();
--- install_app.sh
+++ install_app.sh
@@ -1,13 +1,35 @@
 #!/usr/bin/env sh
 
-if [ "$1" = "-r"  ] || [ "$1" = "--remove" ]; then
-    sudo rm -rf /bin/Anime-Fetch
-    rm -rf ~/.anime-fetch/; 
-else 
-    if [ -f "$HOME/.anime-fetch/" ]; then
+if [ "$1" = "linux" ]; then
+    if [ "$2" = "-r"  ] || [ "$2" = "--remove" ]; then
         sudo rm -rf /bin/Anime-Fetch
         rm -rf ~/.anime-fetch/; 
-    fi 
-    sudo ln -s "$PWD"/build/cli/Anime-Fetch /bin/Anime-Fetch
-    cp -r assets/ ~/.anime-fetch/
+    else 
+        if [ -f "$HOME/.anime-fetch/" ]; then
+            sudo rm -rf /bin/Anime-Fetch
+            rm -rf ~/.anime-fetch/; 
+        fi 
+        sudo ln -s "$PWD"/build/cli/Anime-Fetch /bin/Anime-Fetch
+        cp -r assets/ ~/.anime-fetch/
+    fi
+fi
+if [ "$1" = "freebsd" ]; then
+    if [ "$2" = "-r"  ] || [ "$2" = "--remove" ]; then
+        sudo rm -rf /usr/local/sbin/Anime-Fetch
+        rm -rf ~/.anime-fetch/; 
+    else 
+        if [ -f "$HOME/.anime-fetch/" ]; then
+            sudo rm -rf /usr/local/sbin/Anime-Fetch
+            sudo rm -rf /usr/local/sbin/Anime-Fetch-GUI
+            rm -rf ~/.anime-fetch/; 
+        fi 
+        sudo ln -s "$PWD"/build/cli/Anime-Fetch /usr/local/sbin/Anime-Fetch
+        sudo ln -s "$PWD"/build/gui/Anime-Fetch-GUI /usr/local/sbin/Anime-Fetch-GUI
+        cp -r assets/ ~/.anime-fetch/
+    fi
+else
+    echo "./install_app.sh linux -r : remove linux versiom of the app"
+    echo "./install_app.sh linux : install the linux versiom of the app"
+    echo "./install_app.sh freebsd -r : remove FreeBSD versiom of the app"
+    echo "./install_app.sh freebsd : install the FreeBSD versiom of the app"
 fi
