b1aef9d2f169ec538d32d9fe6bb2e3bca71bb3bc
xgui4 <134389196+xgui4@users.noreply.github.com>
2026-02-22 14:21:56 -0500

    added work in progress bsd support

--- cli/src/services/system_info_service.cpp
+++ cli/src/services/system_info_service.cpp
@@ -1,12 +1,16 @@
 #include "system_info_service.hpp"
 #include "../utils/cmdline_utils.h"
 #include "../utils/utils.hpp"
-#include <hwinfo/cpu.h>
-#include <hwinfo/os.h>
-#include <hwinfo/ram.h>
+
+#ifdef __linux__
+ #include <hwinfo/cpu.h>
+ #include <hwinfo/os.h>
+ #include <hwinfo/ram.h>
+ #include <hwinfo/hwinfo.h>
+#endif
+
 #include <iostream>
 #include <stdexcept>
-#include <hwinfo/hwinfo.h>
 #include <vector>
 
 SystemInfoService::SystemInfoService() {
@@ -25,80 +29,93 @@ std::string SystemInfoService::getSystemInfo() const {
 }
 
 std::string SystemInfoService::getOperatingSystemInfo() const {
-  try {
-    return "OS : " +  hwinfo::OS().name(); 
-  }
-  catch (std::runtime_error error) {
-    std::cout << create_error_str_from_runtime_error(error)<< std::endl;; 
-    return "OS : Unknow"; 
-  }
+  #ifdef __linux__
+    try {
+      return "OS : " +  hwinfo::OS().name(); 
+    }
+    catch (std::runtime_error error) {
+      std::cout << create_error_str_from_runtime_error(error)<< std::endl;; 
+      return "OS : Unknow"; 
+    }
+  #endif
+  return "TBD"; 
 }
 
 std::string SystemInfoService::getKernelName() const {
-  try {
-    return "Kernel : " +  hwinfo::OS().kernel(); 
-  }
-  catch (std::runtime_error error) {
-    std::cout << create_error_str_from_runtime_error(error)<< std::endl;; 
-    return "Kernel : Unknow"; 
-  }
+  #ifdef __linux__
+      try {
+        return "Kernel : " +  hwinfo::OS().kernel(); 
+      }
+      catch (std::runtime_error error) {
+        std::cout << create_error_str_from_runtime_error(error)<< std::endl;; 
+        return "Kernel : Unknow"; 
+      }
+  #endif
+  return "TBD";
 }
 
 std::vector<CPU_INFO> SystemInfoService::getCpuInfo() const {
-  try {
-    const auto cpus = hwinfo::getAllCPUs();
-    std::vector<CPU_INFO> cpus_info = std::vector<CPU_INFO>();  
+  #ifdef __linux__
+    try {
+      const auto cpus = hwinfo::getAllCPUs();
+      std::vector<CPU_INFO> cpus_info = std::vector<CPU_INFO>();  
 
-    for (int i = 0; i > cpus.size() - 1; i++) {
-      if (cpus.at(i).modelName() != cpus.at(i+1).modelName())  {
-          const CPU_INFO cpu = {
-              cpus.at(i).vendor(),
-              cpus.at(i).modelName(),
-              cpus.at(i).numLogicalCores()
-          };
-          cpus_info.push_back(cpu); 
-      }; 
+      for (int i = 0; i > cpus.size() - 1; i++) {
+        if (cpus.at(i).modelName() != cpus.at(i+1).modelName())  {
+            const CPU_INFO cpu = {
+                cpus.at(i).vendor(),
+                cpus.at(i).modelName(),
+                cpus.at(i).numLogicalCores()
+            };
+            cpus_info.push_back(cpu); 
+        }; 
+      }
+      return cpus_info;  
     }
-    return cpus_info;  
-  }
-  catch (std::runtime_error error) {
-    std::cout << create_error_str_from_runtime_error(error) << std::endl; 
-  }
+    catch (std::runtime_error error) {
+      std::cout << create_error_str_from_runtime_error(error) << std::endl; 
+    }
+  #endif
   return std::vector<CPU_INFO> {CPU_INFO { "Error", "Error", 1}};
 }
 
 std::vector<GPU_INFO> SystemInfoService::getGpuInfo() const {
-  try {
-    const auto gpus = hwinfo::getAllGPUs();
-    std::vector<GPU_INFO> gpus_info = std::vector<GPU_INFO>();  
+  #ifdef __linux__
+    try {
+      const auto gpus = hwinfo::getAllGPUs();
+      std::vector<GPU_INFO> gpus_info = std::vector<GPU_INFO>();  
 
-    for (int i = 0; i > gpus.size() - 1; i++) {
-      if (gpus.at(i).name() != gpus.at(i+1).name())  {
-          const GPU_INFO gpu = {
-              gpus.at(i).vendor(),
-              gpus.at(i).name(),
-              std::to_string(gpus.at(i).memory_Bytes())
-          };
-          gpus_info.push_back(gpu); 
-      }; 
+      for (int i = 0; i > gpus.size() - 1; i++) {
+        if (gpus.at(i).name() != gpus.at(i+1).name())  {
+            const GPU_INFO gpu = {
+                gpus.at(i).vendor(),
+                gpus.at(i).name(),
+                std::to_string(gpus.at(i).memory_Bytes())
+            };
+            gpus_info.push_back(gpu); 
+        }; 
+      }
+      return gpus_info;  
     }
-    return gpus_info;  
-  }
-  catch (std::runtime_error error) {
-    std::cout << create_error_str_from_runtime_error(error) << std::endl; 
-  }
+    catch (std::runtime_error error) {
+      std::cout << create_error_str_from_runtime_error(error) << std::endl; 
+    }
+  #endif
   return std::vector<GPU_INFO> {GPU_INFO { "Error", "Error", "Error"}};
 }
 
 std::string SystemInfoService::getMemoryInfo() const {
-  try {
-    long bytes = hwinfo::Memory().total_Bytes(); 
-    return "Memory : " + bytes_to_gigabytes(bytes); 
-  }
-  catch (std::runtime_error error) {
-    std::cout << create_error_str_from_runtime_error(error)<< std::endl;; 
-    return "Kernel : Unknow"; 
-  }
+  #ifdef __linux__
+    try {
+      long bytes = hwinfo::Memory().total_Bytes(); 
+      return "Memory : " + bytes_to_gigabytes(bytes); 
+    }
+    catch (std::runtime_error error) {
+      std::cout << create_error_str_from_runtime_error(error)<< std::endl;; 
+      return "Kernel : Unknow"; 
+    }
+  #endif
+  return "TBD"; 
 }
 
 std::string SystemInfoService::getSwapMemoryInfo() const {
