fd81a798ff239ca6abf10f2528753f6714010501
xgui4 <x134389196+xgui4@users.noreply.github.com>
2026-02-27 19:41:03 -0500

    begin the expriment of vcpkg

--- lib/src/services/system_info_service.cpp
+++ lib/src/services/system_info_service.cpp
@@ -13,6 +13,11 @@
  #include <hwinfo/os.h>
  #include <hwinfo/ram.h>
  #include <hwinfo/hwinfo.h>
+#elif _WIN32
+ #include <hwinfo/cpu.h>
+ #include <hwinfo/os.h>
+ #include <hwinfo/ram.h>
+ #include <hwinfo/hwinfo.h>
 #endif
 
 #include <stdexcept>
@@ -56,6 +61,15 @@ std::string SystemInfoService::getOperatingSystemInfo() const {
       std::cout << create_error_str_from_runtime_error(error)<< std::endl;; 
       return "OS : Unknow"; 
     }
+#elif _WIN32
+	try {
+		return "OS : " + hwinfo::OS().name();
+	}
+	catch (std::runtime_error error) {
+		std::cout << create_error_str_from_runtime_error(error) << std::endl;
+		;
+		return "OS : Unknow";
+	}
   #endif
   #ifdef __unix
     return  "OS : " + exec("uname -ns"); 
@@ -72,8 +86,16 @@ std::string SystemInfoService::getKernelName() const {
         std::cout << create_error_str_from_runtime_error(error) << std::endl;; 
         return "Kernel : Unknow"; 
       }
-  #endif
-  #ifdef __unix
+  #elif _WIN32
+	try {
+		return "Kernel : " + hwinfo::OS().kernel();
+	}
+	catch (std::runtime_error error) {
+		std::cout << create_error_str_from_runtime_error(error) << std::endl;
+		;
+		return "Kernel : Unknow";
+	}
+  #elif __unix
     return  "Kernel : " + exec("uname -Kpr"); 
   #endif
   return "Kernel : Unknown";
@@ -100,6 +122,26 @@ std::vector<CPUInfo> SystemInfoService::getCpuInfo() const {
     catch (std::runtime_error error) {
       std::cout << create_error_str_from_runtime_error(error) << std::endl; 
     }
+  #elif _WIN32
+    try {
+		const auto cpus = hwinfo::getAllCPUs();
+		auto cpus_info = std::vector<CPUInfo>();
+
+		for (int i = 0; i > cpus.size() - 1; i++) {
+			if (cpus.at(i).modelName() != cpus.at(i + 1).modelName()) {
+				const CPUInfo cpu = {
+					.vendor_name = cpus.at(i).vendor(),
+					.model_name = cpus.at(i).modelName(),
+					.logicals_cores = cpus.at(i).numLogicalCores()
+				};
+				cpus_info.push_back(cpu);
+			};
+		}
+		return cpus_info;
+	}
+	catch (std::runtime_error error) {
+		std::cout << create_error_str_from_runtime_error(error) << std::endl;
+	}
   #endif
   #ifdef __FreeBSD__
     std::vector<CPU_INFO> cpu{ 
@@ -135,8 +177,28 @@ std::vector<GPUInfo> SystemInfoService::getGpuInfo() const {
     catch (std::runtime_error error) {
       std::cout << create_error_str_from_runtime_error(error) << std::endl; 
     }
+  #elif _WIN32
+	try {
+		const auto gpus = hwinfo::getAllGPUs();
+		auto gpus_info = std::vector<GPUInfo>();
+
+		for (int i = 0; i > gpus.size() - 1; i++) {
+			if (gpus.at(i).name() != gpus.at(i + 1).name()) {
+				const GPUInfo gpu = {
+					.vendor_name = gpus.at(i).vendor(),
+					.model_name = gpus.at(i).name(),
+					.memory = std::to_string(gpus.at(i).memory_Bytes())
+				};
+				gpus_info.push_back(gpu);
+			};
+		}
+		return gpus_info;
+	}
+	catch (std::runtime_error error) {
+		std::cout << create_error_str_from_runtime_error(error) << std::endl;
+	}
   #endif
-  return std::vector<GPUInfo> {GPUInfo { "Error", "Error", "Error"}};
+  return std::vector<GPUInfo> {GPUInfo {"Error", "Error", "Error"}};
 }
 
 std::string SystemInfoService::getMemoryInfo() const {
@@ -149,13 +211,22 @@ std::string SystemInfoService::getMemoryInfo() const {
       std::cout << create_error_str_from_runtime_error(error)<< std::endl;; 
       return "Kernel : Unknow"; 
     }
+  #elif _WIN32
+	try {
+		long bytes = hwinfo::Memory().total_Bytes();
+		return "Memory : " + bytes_to_gigabytes(bytes);
+	}
+	catch (std::runtime_error error) {
+		std::cout << create_error_str_from_runtime_error(error) << std::endl;
+		;
+		return "Kernel : Unknow";
+	}
   #endif
   #ifdef __FreeBSD__
     return  "Memory : " + exec("sysctl -n hw.realmem"); 
   #endif
-  return "TBD"; 
-}
-
+   return "TBD";     
+ }
 std::string SystemInfoService::getSwapMemoryInfo() const {
   throw std::runtime_error("Not Implemented Yet");
 }
