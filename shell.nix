6d5ee380a67d93f4caebf098a54240b2d476ecc3
xgui4 <134389196+xgui4@users.noreply.github.com>
2026-06-21 20:43:07 -0400

    update nix support

--- shell.nix
+++ shell.nix
@@ -15,21 +15,11 @@ pkgs.mkShell rec {
 
   postInstall = ''
     # Desktop Entry & Mime
-    install -Dm644 "packages/io.github.xgui4.lce_qt_launcher.desktop" "$out/share/applications/io.github.xgui4.lce_qt_launcher.desktop"
-    install -Dm644 "packages/lce_inst-mime.xml" "$out/share/mime/packages/io.github.xgui4.lce_qt_launcher.xml"
-    install -Dm644 "packages/io.github.xgui4.lce_qt_launcher.metainfo.xml" "$out/share/metainfo/io.github.xgui4.lce_qt_launcher.metainfo.xml"
-
-    # Icons
-    install -Dm644 "assets/io.github.xgui4.lce_qt_launcher.png" "$out/share/icons/hicolor/128x128/apps/io.github.xgui4.lce_qt_launcher.png"
-    install -Dm644 "assets/lce_inst.png" "$out/share/icons/hicolor/128x128/mimetypes/application-x-lce_inst.png"
-
-    # Data/Assets (Mirroring your /opt logic into Nix share)
-    mkdir -p "$out/share/${pname}"
-    cp -r data assets "$out/share/${pname}/"
+    install -Dm644 "packages/anime-fetch-gui.desktop" "$out/share/applications/anime-fetch-gui.desktop"
 
     # Docs
-    install -Dm644 "license.md" -t "$out/share/licenses/${pname}/"
-    install -Dm644 "readme.md" "code-of-conduct.md" -t "$out/share/doc/${pname}/"
+    install -Dm644 "LICENSE.md" -t "$out/share/licenses/${pname}/"
+    install -Dm644 "README.md" "code-of-conduct.md" -t "$out/share/doc/${pname}/"
   '';
 
   dontCheckRuntimeDeps = true;
